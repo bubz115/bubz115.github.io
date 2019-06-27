085628b94dfcc101dec3d72b3210bda747a00e0d
bubz <bublu5a@gmail.com>
2019-06-28 00:17:36 +0500

    Updates

---
+++ precache-manifest.5daf275256a540467a6e13c9ded9da56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "bbe08367ecdf1d2e0c84",
+    "url": "/myportfolio/static/js/main.bbe08367.chunk.js"
+  },
+  {
+    "revision": "1be88bf33335f4346bb4",
+    "url": "/myportfolio/static/js/1.1be88bf3.chunk.js"
+  },
+  {
+    "revision": "bbe08367ecdf1d2e0c84",
+    "url": "/myportfolio/static/css/main.d8b73908.chunk.css"
+  },
+  {
+    "revision": "1be88bf33335f4346bb4",
+    "url": "/myportfolio/static/css/1.f328dbc8.chunk.css"
+  },
+  {
+    "revision": "215e8e567cf0eba3f68c6f3c62da4595",
+    "url": "/myportfolio/index.html"
+  }
+];
